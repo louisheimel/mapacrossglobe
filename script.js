2a7bd61c6862c929ddc9b488eb672e79a7503a56
louisheimel1@gmail.com <louisheimel1@gmail.com>
2017-06-23 13:34:51 -0500

    add basic mercator map

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 fetch('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json')
 .then(data => data.json())
 .then(data => {
+  const config = {
+    // code adapted from http://bl.ocks.org/almccon/fe445f1d6b177fd0946800a48aa59c71
+    width: 960,
+    height: 500,
+    margin: '0 auto',
+    display: 'block',
+  }
+  var svg = d3.select('body').append('svg')
+
+  var projection = d3.geoMercator()
+    .scale(config.width / 2 / Math.PI)
+    .translate([config.width / 2, config.height / 2])
+
+  var path = d3.geoPath()
+    .projection(projection)
+
+  var url = 'http://enjalot.github.io/wwsd/data/world/world-110m.geojson'
+  d3.json(url, (err, geojson) => {
+    svg
+      .style('width', config.width)
+      .style('height', config.height)
+      .style('margin', config.margin)
+      .style('display', config.display)
+      .append('path')
+      .attr('d', path(geojson))
+  })
   console.log(data)
 })
